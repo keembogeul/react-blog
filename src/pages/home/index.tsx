e41ff90f5ba1255bf3476444ecbb59d79f18ff0a
keembogeul <318qhtmf@gmail.com>
2023-10-13 15:21:24 +0900

    게시글 리스트 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,7 +10,21 @@ export default function Home() {
           <Link to="/profile">프로필</Link>
         </div>
       </header>
-      <div className="post-list">post list</div>
+      <div className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <div key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post_profile-box">
+                <div className="post__profile"></div>
+                <div className="post__author-name">패스트 캠퍼스</div>
+                <div className="post__date">2023.07.08 토요일</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            </Link>
+          </div>
+        ))}
+      </div>
       <footer>
         <div>Menu 1</div>
         <div>Menu 2</div>
